b04d830b0d11a4fb9f5e78acec3e3a05b802af12
rverat <thedevlair@gmail.com>
2026-05-20 22:47:56 -0500

    chore: initial commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("registroForm");
+
+form.addEventListener("submit",function(e){
+
+e.preventDefault();
+
+alert("Cuenta creada correctamente.");
+
+});
---
+++ js/dashboard.js
@@ -0,0 +1,17 @@
+const fecha =
+new Date();
+
+document.getElementById("fecha").innerHTML =
+fecha.toLocaleString();
+
+const menuBtn =
+document.getElementById("menuBtn");
+
+const sidebar =
+document.getElementById("sidebar");
+
+menuBtn.addEventListener("click",()=>{
+
+sidebar.classList.toggle("show");
+
+});
